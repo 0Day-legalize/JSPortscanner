4367ad5679c8d3cd7877add1a06bcc4d9c072446
Jamie Heyckendorf <Jamie.Heyckendorf@DACS-BUND.de>
2026-06-02 10:57:26 +0200

    add shuffle

--- archive/shuffle.js
+++ archive/shuffle.js
@@ -1,5 +1,18 @@
-//import
-function shuffle(beginning_port, end_port){
-    // math random 
+
+function shuffle(beginning_port, end_port) {
+
+    let zahlen = [];
+
+    while (zahlen.length < (end_port - beginning_port + 1)) {
+
+        let rnd = Math.floor(Math.random() * (end_port - beginning_port + 1)) + beginning_port;
+
+        if (!zahlen.includes(rnd)) {
+            zahlen.push(rnd);
+        }
+    }
+
+    console.log(zahlen);
 }
 
+shuffle(1, 100);
